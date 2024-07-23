13b66d75ba3256415d1b55882cfe5a2a941808d2
Josh Mayanja <Joshua.Mayanja@nuix.com>
2024-07-23 00:11:04 -0400

    Began model building

--- Models/RandomForest/main.py
+++ Models/RandomForest/main.py
@@ -1,12 +1,12 @@
 from random_forest import RandomForest
 from Data.columns import dependent_variables
 
-DEPENDANT_VARIABLES = [i for i in dependent_variables.keys()]
+DEPENDENT_VARIABLES = [i for i in dependent_variables.keys()]
 
 
 if __name__ == "__main__":
 
-    for variable in DEPENDANT_VARIABLES:
+    for variable in DEPENDENT_VARIABLES:
 
         # Initialize Random Forest model
         model = RandomForest(variable)
